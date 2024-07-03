59ae1e78149f708dd744ac016b757cf666e9ca18
Panpak Apesariyayo <ingpanpak@gmail.com>
2024-07-03 11:24:18 +0700

    nested-loop

--- ex-2.js
+++ ex-2.js
@@ -1,4 +1,16 @@
-// เริ่มเขียนโค้ดตรงนี้ 
-function printStar() {
-   
+// เริ่มเขียนโค้ดตรงนี้
+function printStar(num) {
+  let result = "";
+  for (let i = 1; i < num + 1; i++) {
+    let row = "";
+    for (let j = 0; j < i; j++) {
+      row += "* ";
+    }
+    result += row + "\n";
+  }
+  return result;
 }
+
+let test02 = printStar(5);
+
+console.log(test02);
--- ex-3.js
+++ ex-3.js
@@ -1,4 +1,17 @@
-// เริ่มเขียนโค้ดตรงนี้ 
-function multiplicationTable() {
-   
+// เริ่มเขียนโค้ดตรงนี้
+function multiplicationTable(num) {
+  let result = "";
+  for (let i = 1; i < num + 1; i++) {
+    let row = "";
+    for (let j = 1; j < num + 1; j++) {
+      let num02 = j * i;
+      row += `${num02} `;
+    }
+    result += row + "\n";
+  }
+  return result;
 }
+
+let test03 = multiplicationTable(5);
+
+console.log(test03);
